868c984554aa50b77551167a9a5aa0b2696b05d4
kgothatsom <kgothatsom@digikonect.co.za>
2021-01-17 11:57:50 +0200

    Adding adding all nessecarry js files and type effect

--- js/main.js
+++ js/main.js
@@ -2,9 +2,16 @@
 jQuery(document).ready(function(){
  "use strict"
     $('.slider').ripples({
-      dropRadius: 11,
+      dropRadius: 7,
       perturbance: 0.01,
        
     });
+  
+     $(".text").typed({
+        strings:["<strong>i love</strong><strong class='primary'> coding.</strong>","<strong>and to</strong><strong class='primary'> share !!.</strong>"],
+         typespeed:0,
+         loop:true
+        
+     });
   
 });
